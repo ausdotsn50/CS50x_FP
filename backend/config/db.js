30710cf387472659b93445668a5c2852078b68f2
ausdotsn50 <ausdotsn50@gmail.com>
2025-07-14 15:52:52 +0800

    backend as submodule

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-// creating connection to database
-import "dotenv/config";
-
-import {neon} from "@neondatabase/serverless";
-
-// creates a sql connection using DB_URL
-export const sql = neon(process.env.DATABASE_URL);
