2b796efc81db1d436cfa0a54a8d9e6d97fd5c78b
ausdotsn50 <ausdotsn50@gmail.com>
2025-07-15 10:50:05 +0800

    fetching orders and summary draft code

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -29,9 +29,12 @@ export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-      <Text>Order made by: {orders[0].user_id} </Text>
-      <Text>Order type: {orders[0].type} </Text>
-      <Text>Order created at: {summary[0].created_at}</Text>
+      <Text>Order Information</Text>
+      <Text>Name: {orders[0].name}</Text>
+      <Text>Address: {orders[0].address}</Text>
+      <Text>Order type: {orders[0].type}</Text>
+      <Text>Number of orders: {orders[0].quantity}</Text>
+      <Text>Total amount owed: {orders[0].base_price * orders[0].quantity}</Text>
       <SignOutButton />
     </View>
   );
