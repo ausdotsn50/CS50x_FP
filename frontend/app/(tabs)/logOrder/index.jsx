84ddfc1b0b04a5702534c7e597b1155bcca3b6ef
ausdotsn50 <ausdotsn50@gmail.com>
2025-07-19 22:53:44 +0800

    search functionality

--- frontend/app/(tabs)/logOrder/index.jsx
+++ frontend/app/(tabs)/logOrder/index.jsx
@@ -1,25 +1,38 @@
+import filter from "lodash.filter";
 import PageLoader from '@/components/PageLoader';
-import { FlatList, TextInput, Text, View } from 'react-native';
+
+import { CustomersItem } from '@/components/CustomersItem';
+import { FlatList, Text, TextInput, View } from 'react-native';
 import { styles } from "@/assets/styles/logOrder.styles.js";
 import { genStyles } from '@/assets/styles/general.styles.js';
-import { useState } from 'react';
-import { useCustomers } from "@/hooks/useCustomers.js";
-import { useUser } from '@clerk/clerk-expo';
-import { CustomersItem } from '@/components/CustomersItem';
-import { useEffect  } from 'react';
 import { handleDelete } from '@/utils/helpers';
+import { useCustomers } from "@/hooks/useCustomers.js";
+import { useEffect  } from 'react';
 import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import { useUser } from '@clerk/clerk-expo';
 
 export default function LogOrder() {
-    const[searchQuery, setSearchQuery] = useState(""); // by default, an empty string
-    
     const { user } = useUser();
     const { customers, isLoading, loadData, deleteCustomer } = useCustomers(user.id)
+
+    const[searchQuery, setSearchQuery] = useState(""); // by default, an empty string
+    const[filteredCustomers, setFilteredCustomers] = useState([]); // stored filtered query of customers here
 
     const router = useRouter();
     
     const handleSearch = (query) => {
-        setSearchQuery(query);
+        setSearchQuery(query); // searchQuery value changes via setSearchQuery
+        
+        const formattedQuery = query.toLowerCase(); // query in lower case
+        const filteredData = filter(customers, (customer) => { 
+            return contains(customer, formattedQuery)
+        })
+        setFilteredCustomers(filteredData);
+    }
+
+    const contains = (cm, query) => { // customer compared to query
+        return cm.name?.toLowerCase().includes(query); // lower case to lower case comparison
     }
 
     const createOrder = () => {
@@ -51,7 +64,7 @@ export default function LogOrder() {
             <FlatList
                 style={genStyles.itemsList}
                 contentContainerStyle={genStyles.itemsListContent}
-                data={customers}
+                data={filteredCustomers}
                 renderItem={({item}) => (
                     // Choose customer op
                     // To do: choose products for customer purchase
